d8b6ee824a754773dda39f860a4ee56835fda6a5
Ashish Bhingole <ashishbhingole7@gmail.com>
2026-08-23 18:18:20 +0530

    Add customer Ingestion Script

---
+++ src/ingestion.py
@@ -0,0 +1,8 @@
+def ingest_customer_data():
+    print("Starting customer data ingestion")
+    print("Reading customer data from source")
+    print("Customer data ingestion completed")
+
+
+if __name__ == "__main__":
+    ingest_customer_data()
